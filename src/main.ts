70e5bd02952162186f68486285026fc938bc33f0
gdeelder <126334723+gdeelder@users.noreply.github.com>
2023-02-26 10:35:34 +0100

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,22 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-    
+    <h1>Weclome to {{name}}</h1>
+    <mat-tab-group>
+    <mat-tab label="First"> Content 1 </mat-tab>
+    <mat-tab label="Second"> Content 2 </mat-tab>
+  </mat-tab-group>
   `,
 })
-export class App {
-  name = 'Angular';
+export class PackApp {
+  name = 'PackApp';
 }
 
-bootstrapApplication(App);
+bootstrapApplication(PackApp);
